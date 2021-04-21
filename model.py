71196ef7af60613ac46f5a06a559005db629e609
cvogler722 <54826492+cvogler722@users.noreply.github.com>
2021-04-21 18:28:40 -0400

    bye bye tensorboard

--- model.py
+++ model.py
@@ -6,58 +6,12 @@ from preprocess import preprocess_fn
 import hyperparameters as hp
 import os
 from datetime import datetime
-from tensorboard import ImageLabelingLogger, ConfusionMatrixLogger, CustomModelSaver
 
 
 def retrieve_saved_model():
     model = tf.keras.models.load_model('./trained_model/model1.h5')
     model.summary()
     return model
-
-
-def train_and_test_model():
-    train_data, train_labels, test_data, test_labels = preprocess_fn()
-    model = get_model()
-    model.summary()
-
-    time_now = datetime.now()
-    timestamp = time_now.strftime("%m%d%y-%H%M%S")
-
-    checkpoint_path = "checkpoints" + os.sep + \
-        "your_model" + os.sep + timestamp + os.sep
-    logs_path = "logs" + os.sep + "your_model" + \
-        os.sep + timestamp + os.sep
-
-    idx_to_class = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'K',
-                       10: 'L', 11: 'M', 12: 'N', 13: 'O', 14: 'P', 15: 'Q', 16: 'R', 17: 'S',
-                       18: 'T', 19: 'U', 20: 'V', 21: 'W', 22: 'X', 23: 'Y'}
-
-    callback_list = [
-        tf.keras.callbacks.TensorBoard(
-            log_dir=logs_path,
-            update_freq='batch',
-            profile_batch=0),
-        ImageLabelingLogger(logs_path, train_data, idx_to_class),
-        CustomModelSaver(checkpoint_path, hp.max_num_weights)
-    ]
-
-    model.compile(
-        optimizer='adam',
-        loss='sparse_categorical_crossentropy',
-        metrics=["sparse_categorical_accuracy"])
-
-    model.fit(
-        x=train_data,
-        y=train_labels,
-        validation_data=(test_data, test_labels),
-        epochs=hp.num_epochs,
-        callbacks=callback_list,
-    )
-    model.evaluate(
-        x=test_data,
-        y=test_labels,
-        verbose=1
-    )
 
 
 def train_and_save_model():
